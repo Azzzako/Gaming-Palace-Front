1069dd841cb10376c84ee74e67c3847ca6f0eb1a
Fatima Ponce <faatii.ponce@gmail.com>
2023-02-03 13:35:18 -0300

    landing, corregir errores

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-import React from 'react';
-import { Routes, Route} from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar'
-import Home from './components/Home/Home';
-import Detail from './components/Detail/Detail'
-import InConstructionPage from './components/InconstructionPage/InConstructionPage';
-import "./App.css"
-import { Route, Routes } from 'react-router-dom';
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Detail from "./components/Detail/Detail";
+import InConstructionPage from "./components/InconstructionPage/InConstructionPage";
+import LandingPage from "./components/LandingPage/LandingPage";
+import "./App.css";
 
 function App() {
   return (
-
     <div className="App">
-
-     <Navbar/>
-					<Routes>
-					
-
-
-					
-
-          <Route exact path='/' element={<Home/>}/>
-          <Route exact path='/myprofile' element={<InConstructionPage/>}/>
-					<Route exact path='/detail/:id' element={<Detail/>}/>
-						
-					
-
-					</Routes>
-				
-
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/myprofile" element={<InConstructionPage />} />
+        <Route exact path="/detail/:id" element={<Detail />} />
+        <Route exact path="/landing" element={<LandingPage />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
-
---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../LandingPage/LandingPage.module.css";
+
+function LandingPage() {
+  return (
+    <div id={styles.background}>
+      <div id={styles.container}>{/* <h1>Welcome to Gaming Palace</h1> */}</div>
+      <Link to="/">
+        <button id={styles.button}>Start!</button>
+      </Link>
+    </div>
+  );
+}
+
+export default LandingPage;
