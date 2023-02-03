14a9d1c2f96a23b38910e583e678d9949f5ae06a
EliasPiolatto <eliaspiolatto77@hotmail.com>
2023-02-03 00:09:15 -0300

    estilos de las cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar'
 import Home from './components/Home/Home';
 import Detail from './components/Detail/Detail'
 import InConstructionPage from './components/InconstructionPage/InConstructionPage';
 import "./App.css"
-import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
