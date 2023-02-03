c52668b4dca4257e5160e6932e36951a27242da7
massavalentina <valecacciavi@gmail.com>
2023-02-03 16:39:02 -0300

    corregido

--- src/Redux/Actions/constants.js
+++ src/Redux/Actions/constants.js
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import axios from "axios"
-export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS"
-
-
-export const getAllProducts = () => {
-    return async function (dispatch) {
-        const response = await axios.get(`http://localhost:3001/products`)
-        return dispatch({
-            type: GET_ALL_PRODUCTS,
-            payload: response.data
-        })
-    }
-}
-=======
-export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
->>>>>>> DEVELOP
-=======
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
 export const GET_DETAIL = "GET_DETAIL";
 export const POST_PRODUCT = "POST_PRODUCT";
-export const CLEAN_DETAIL = "CLEAN_DETAIL";
-export const SET_LOADING = "SET_LOADING";
->>>>>>> DEVELOP
+
