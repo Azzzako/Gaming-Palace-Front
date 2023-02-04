cfa11eb10555d3dd48934025618189303be8fb6c
EliasPiolatto <eliaspiolatto77@hotmail.com>
2023-02-03 21:36:45 -0300

    footer no pulido

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import './Footer.css';
+import { Link } from "react-router-dom";
+
+
+export default function Footer() {
+  const navigate = useNavigate();
+  return (
+  
+    <Navbar className="footer" bg="dark" variant="dark">
+      <Navbar.Text className='Light-Font'>© GamingPalace E-Commerce, 2023. All rights reserved.</Navbar.Text>
+      <Navbar.Text >
+        <Link to='/myprofile'>
+          <button className="btn btn-danger text-decoration-none">About us</button>
+        </Link>
+      </Navbar.Text >
+      <Navbar.Text>
+        <Link to='/myprofile'>
+          <button className="btn btn-danger text-decoration-none">Contact us</button>
+        </Link>
+      </Navbar.Text>
+      <Navbar.Text>
+      </Navbar.Text>
+    </Navbar>
+  )
+}
