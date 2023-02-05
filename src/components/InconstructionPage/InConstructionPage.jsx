4497eaba829ac37a7550717ce51ed716d082d7d4
fvaldezz96 <valdezfede21@gmail.com>
2023-02-05 18:43:01 +0100

    espacios

--- src/components/InconstructionPage/InConstructionPage.jsx
+++ src/components/InconstructionPage/InConstructionPage.jsx
@@ -4,15 +4,10 @@ import s from './InConstructionPage.module.css'
 
 
 export default function InConstructionPage() {
-    
+
     return (
-
-      <div >
-
-          <h1 className={s.Avise}>Page in construction ⚙️</h1>
-
-      </div>
-      
-  
-	);
+        <div >
+            <h1 className={s.Avise}>Page in construction ⚙️</h1>
+        </div>
+    );
 };
