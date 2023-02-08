ad5e9704a002c9fa8c8d78e0a6a7654ef10fa168
EliasPiolatto <eliaspiolatto77@hotmail.com>
2023-02-07 21:39:01 -0300

    in process......

--- src/components/ShopCart/CardCart.jsx
+++ src/components/ShopCart/CardCart.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BsTrash2Fill } from 'react-icons/bs';
+import { useDispatch } from 'react-redux'
+import { deleteCart } from '../../Redux/Actions/actions';
 
 const CardCart = ({image, name, price, stock, id}) => {
+
+const dispatch = useDispatch();
+
+const handleDeleteCart = (id) => {
+  dispatch(deleteCart(id))
+}
+
   return (
     <div>
+        <button className='btn-del-fav' onClick={()=>handleDeleteCart(id)}>{<BsTrash2Fill/>}</button>
+
         <img src={image} alt='*' width='150px' height='100px'/>
         <h2>{name}</h2>
         <h4>US$ {price}</h4>
