bf0c9a514c585ffa37b6f941de20355cebc18172
Fatima Ponce <faatii.ponce@gmail.com>
2023-02-12 22:03:57 -0300

    dasjk

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,59 +1,59 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import './Card.css'
-import { BsCartFill, BsHeartFill } from 'react-icons/bs';
-import { useDispatch, useSelector } from 'react-redux';
-import { addFav, deleteFavs } from '../../Redux/Actions/actions.js';
+import "./Card.css";
+import { BsCartFill, BsHeartFill } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { addFav, deleteFavs } from "../../Redux/Actions/actions.js";
 
-
-
-const Card = ({image, price, name, description, id}) => {
-
-  const favourites = useSelector(state=> state.favourites)
+const Card = ({ image, price, name, description, id }) => {
+  const favourites = useSelector((state) => state.favourites);
   const disptach = useDispatch();
-  const existFavs = favourites.map(fav => fav.id)
+  const existFavs = favourites.map((fav) => fav.id);
 
   const handleFav = (id) => {
-    !existFavs.includes(id) ?
-    disptach(addFav(id)) :
-    disptach(deleteFavs(id))
+    !existFavs.includes(id) ? disptach(addFav(id)) : disptach(deleteFavs(id));
   };
 
   return (
-    <div className='cards'>
+    <div className="cards">
+      <div className="img-icons">
+        <Link to={`/detail/${id}`}>
+          <img src={image} alt="*" width="150px" height="100px" />
+        </Link>
 
-      <div className='img-icons'>
-
-      <Link to={`/detail/${id}`}>
-      <img src={image} alt='*' width='150px' height='100px'/>
-      </Link>
-
-        <div className='icons'>
-          {
-            existFavs.includes(id) ? <BsHeartFill color='red' className='icons-fav' onClick={()=>{handleFav(id)}}/>
-            : <BsHeartFill className='icons-fav' onClick={()=>{handleFav(id)}}/>
-          }     
+        <div className="icons">
+          {existFavs.includes(id) ? (
+            <BsHeartFill
+              color="red"
+              className="icons-fav"
+              onClick={() => {
+                handleFav(id);
+              }}
+            />
+          ) : (
+            <BsHeartFill
+              className="icons-fav"
+              onClick={() => {
+                handleFav(id);
+              }}
+            />
+          )}
         </div>
 
-          <Link to="/myprofile">
-        <div className='icons'>
-          <BsCartFill className='icons-cart'/>
-        </div>
-          </Link>
-          
+        <Link to="/myprofile">
+          <div className="icons">
+            <BsCartFill className="icons-cart" />
+          </div>
+        </Link>
       </div>
-      
 
       <h3>{name}</h3>
 
       <h3>US$ {price}</h3>
 
       {/* <h3>{description}</h3> */}
-
     </div>
-  )
+  );
 };
 
-export default Card
-
-
+export default Card;
--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,122 +1,136 @@
-import React, { useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getDetail } from '../../Redux/Actions/actions'
-import { Link } from 'react-router-dom';
-import { BsHeartFill } from 'react-icons/bs';
-import { addFav, deleteFavs } from '../../Redux/Actions/actions.js';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getDetail } from "../../Redux/Actions/actions";
+import { Link } from "react-router-dom";
+import { BsHeartFill } from "react-icons/bs";
+import { addFav, deleteFavs } from "../../Redux/Actions/actions.js";
 
-import Button from 'react-bootstrap/Button';
-
-
-
+import Button from "react-bootstrap/Button";
 
 const Detail = (props) => {
+  const detail = useSelector((i) => i.details);
 
-
-	const detail = useSelector((i) => i.details);
-
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const { id } = useParams();
-	const { history } = props; 
-  const favourites = useSelector(state=> state.favourites)
+  const { history } = props;
+  const favourites = useSelector((state) => state.favourites);
 
-
-  const existFavs = favourites.map(fav => fav.id)
-	useEffect(() => {
-		dispatch(getDetail(id));
-	},[]);
+  const existFavs = favourites.map((fav) => fav.id);
+  useEffect(() => {
+    dispatch(getDetail(id));
+  }, []);
 
   const handleFav = (id) => {
-    !existFavs.includes(id) ?
-    dispatch(addFav(id)) :
-    dispatch(deleteFavs(id))
+    !existFavs.includes(id) ? dispatch(addFav(id)) : dispatch(deleteFavs(id));
   };
 
-	const goHome = () => {
-		history.goHome();
-	};
+  const goHome = () => {
+    history.goHome();
+  };
 
-	return (
-
-		<div className='bkg'>
-			
-		<div>
-		
-
-			<div >
-				{detail && 
-					
-          <div className='container align-items-center p-5 mt-5'>
-            <Link to={"/home"}>
-            <button className="button btn btn-secondary" onClick={goHome}>HOME {'> '}{detail.category} </button></Link>
-	          <div className="row detailsContainer d-flex flex-column align-items-center">
-              <div className="card row detailsContainer d-flex flex-column align-items-center">
-              <div className=" col-12 d-flex flex-sm-column flex-md-row align-items-center justify-content-center">
-                <div className="d-flex img-fluid justify-content-center">
-                <img src={detail.imageurl}  className="img-fluid" alt="img" />
-                </div>
-
-                <div className="container-title">
-                  <div className="container p-3 d-flex flex-column align-items-start justify-content-around border-start border-dark border-opacity-10 my-5">
-
-                    <div className="d-flex flex-column align-items-start justify-content-around" >
-                          <div className='d-grid gap-3 col-10 mx-auto'>
-                          <h6 className='text-muted'>{detail.trademark}</h6>
-                          <h2 className="d-flex flex-column align-items-start text-uppercase tx4 my-3">{detail.name}</h2>
-                          <h6 className='text-muted'>{detail.description}</h6>
-                          </div>
+  return (
+    <div className="bkg">
+      <div>
+        <div>
+          {detail && (
+            <div className="container align-items-center p-5 mt-5">
+              <Link to={"/home"}>
+                <button className="button btn btn-secondary" onClick={goHome}>
+                  HOME {"> "}
+                  {detail.category}{" "}
+                </button>
+              </Link>
+              <div className="row detailsContainer d-flex flex-column align-items-center">
+                <div className="card row detailsContainer d-flex flex-column align-items-center">
+                  <div className=" col-12 d-flex flex-sm-column flex-md-row align-items-center justify-content-center">
+                    <div className="d-flex img-fluid justify-content-center">
+                      <img
+                        src={detail.imageurl}
+                        className="img-fluid"
+                        alt="img"
+                      />
                     </div>
 
-                      {/* HACER RESPONSIVES BOTONES */}
-                      <div class=" form-outline item col-10 mx-auto " >
-                          <h3 className='my-3'>US$ {detail.price} </h3>
-                          <div className='d-flex gap-2 my-3'>
-                            <input type="number" min="1" max="10" class="form-control form" style={{width: '5rem'}} />
-
-
-
-                            <div className='icons'>
-
-                              {
-                                existFavs.includes(detail.id) ? <BsHeartFill color='red' className='icons-fav' onClick={()=>{handleFav(detail.id)}}/>
-                                : <BsHeartFill color='lightslategray' className='icons-fav' onClick={()=>{handleFav(detail.id)}}/>
-                              }     
-                           </div>
-
-
+                    <div className="container-title">
+                      <div className="container p-3 d-flex flex-column align-items-start justify-content-around border-start border-dark border-opacity-10 my-5">
+                        <div className="d-flex flex-column align-items-start justify-content-around">
+                          <div className="d-grid gap-3 col-10 mx-auto">
+                            <h6 className="text-muted">{detail.trademark}</h6>
+                            <h2 className="d-flex flex-column align-items-start text-uppercase tx4 my-3">
+                              {detail.name}
+                            </h2>
+                            <h6 className="text-muted">{detail.description}</h6>
                           </div>
+                        </div>
 
+                        {/* HACER RESPONSIVES BOTONES */}
+                        <div class=" form-outline item col-10 mx-auto ">
+                          <h3 className="my-3">US$ {detail.price} </h3>
+                          <div className="d-flex gap-2 my-3">
+                            <input
+                              type="number"
+                              min="1"
+                              max="10"
+                              class="form-control form"
+                              style={{ width: "5rem" }}
+                            />
+
+                            <div className="icons">
+                              {existFavs.includes(detail.id) ? (
+                                <BsHeartFill
+                                  color="red"
+                                  className="icons-fav"
+                                  onClick={() => {
+                                    handleFav(detail.id);
+                                  }}
+                                />
+                              ) : (
+                                <BsHeartFill
+                                  color="lightslategray"
+                                  className="icons-fav"
+                                  onClick={() => {
+                                    handleFav(detail.id);
+                                  }}
+                                />
+                              )}
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="d-grid gap-2 col-10 mx-auto">
+                          <Link to={"/inconstruction"}>
+                            <button
+                              type="button"
+                              class="btn btn-outline-secondary my-2"
+                              style={{ width: "15rem" }}
+                            >
+                              Shop Now
+                            </button>
+                          </Link>
+
+                          <Link to={"/inconstruction"}>
+                            <button
+                              type="submit"
+                              className="button btn btn-secondary my-2"
+                              style={{ width: "15rem" }}
+                            >
+                              Add to cart
+                            </button>
+                          </Link>
+                        </div>
                       </div>
-
-
-                      
-                    <div className='d-grid gap-2 col-10 mx-auto'>
-                    <Link to={"/inconstruction"}>
-                        <button type="button" class="btn btn-outline-secondary my-2" style={{width: '15rem'}}>Shop Now</button>
-                    </Link>
-
-                    <Link to={"/inconstruction"}>
-                        <button type="submit" className="button btn btn-secondary my-2" style={{width: '15rem'}}>Add to cart</button>
-                    </Link>
                     </div>
-                      
-
-
-
                   </div>
                 </div>
-							</div>
-						</div>
-					  </div>
-          </div>
-				}
-			
-		</div>
-		</div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Detail;
