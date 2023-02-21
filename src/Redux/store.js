0dc4f58e0884cf542a6730ba5a383417c121c89c
Asael <zanduva@gmail.com>
2023-02-20 23:19:00 -0600

    persist

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,7 +3,10 @@ import rootReducer from './reducer';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
 import thunk from 'redux-thunk';
-// import { createLogger } from 'redux-logger';
+import { createLogger } from 'redux-logger';
+
+
+const loggerMiddleware = createLogger();
 
 const persistConfig = {
   key: 'root',
@@ -16,7 +19,7 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   persistedReducer,
-  composeEnhancer(applyMiddleware(thunk))
+  composeEnhancer(applyMiddleware(thunk, loggerMiddleware))
 );
 
 const persistor = persistStore(store);
